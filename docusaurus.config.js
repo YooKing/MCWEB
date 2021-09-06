2afa8751109fbbef7382f410c10d39dbb54d9305
Yooking <wangyukun55555@gmail.com>
2021-09-06 14:41:58 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,13 +22,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/YooKing/MCWEB/edit/documentation/docs/',
+          editUrl: 'https://github.com/YooKing/MCWEB/edit/documentation/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/YooKing/MCWEB/edit/documentation/blog/',
+            'https://github.com/YooKing/MCWEB/edit/documentation/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
