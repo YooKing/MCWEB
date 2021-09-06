fa248ebbba634d92e60f4d6cf1964038ef256045
Yooking <wangyukun55555@gmail.com>
2021-09-06 17:57:57 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,12 +12,12 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">已经俩年的公益MC服务器</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            服务器介绍
           </Link>
         </div>
       </div>
