8b5c77f742d4b344ac0a6db724becf99499d9034
Yooking <wangyukun55555@gmail.com>
2021-09-06 17:13:42 +0800

    Update HomepageFeatures.js

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,32 +4,30 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Step 1.',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        加入QQ群497912971
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Step 2.',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        下载客户端(可用纯净版)提取码:bqdh,https://pan.baidu.com/s/1xU7rJIekhkwQJLor5fPVQw
+
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Step 3.',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        查看公告
       </>
     ),
   },
